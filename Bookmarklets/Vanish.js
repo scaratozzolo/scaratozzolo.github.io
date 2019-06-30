a4ffd7a4d8037b8d5cda0b05ebcb6aa432501ad0
Scott Caratozzolo <scaratozzolo12@gmail.com>
2019-06-30 16:56:29 -0400

    Create Vanish.js

---
+++ Bookmarklets/Vanish.js
@@ -0,0 +1,22 @@
+var start = document.getElementsByTagName("html")[0];
+var elements = [];
+
+for(var i = 0; i < start.children.length; i++){
+	
+	getChildren(start.children[i]);	
+
+}
+
+
+function getChildren(e){
+	if(e.children != 0){
+		for(int j = 0; j < e.children.length; j++){
+			getChildren(e.children[j]);
+		}
+	}else{
+		elements.push(e);
+	}
+
+}
+
+console.log(elements);
