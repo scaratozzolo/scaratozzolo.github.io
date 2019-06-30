17b8f6a71b45d48f8a7bc361f899e0dbcb10959c
Scott Caratozzolo <scaratozzolo12@gmail.com>
2019-06-30 14:56:14 -0400

    Update Work-Clicker.js

--- Bookmarklets/Work-Clicker.js
+++ Bookmarklets/Work-Clicker.js
@@ -1,3 +1,30 @@
+var v = "3.4.1";
+
+if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+    var done = false;
+    var script = document.createElement("script");
+    script.src = "https://code.jquery.com/jquery-" + v + ".js";
+    script.onload = script.onreadystatechange = function(){
+        if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+            done = true;
+            initMyBookmarklet();
+        }
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+} else {
+    initMyBookmarklet();
+}
+
+
+function initMyBookmarklet() {
+  setInterval(function(){ eventFire(document.getElementById('bigCookie'), 'click'); }, 50);
+  
+  $(document).keydown(function(e){
+	  alert(e.keyCode);
+  });
+}
+
+
 function eventFire(el, etype){
   if (el.fireEvent) {
     el.fireEvent('on' + etype);
@@ -8,4 +35,4 @@ function eventFire(el, etype){
   }
 }
 
-setInterval(function(){ eventFire(document.getElementById('bigCookie'), 'click'); }, 50);
+
