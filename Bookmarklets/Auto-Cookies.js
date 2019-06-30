2df0ed0674e02d738052a3b187f266eacf5e4710
Scott Caratozzolo <scaratozzolo12@gmail.com>
2019-06-30 13:52:57 -0400

    Create Auto-Cookies.js

---
+++ Bookmarklets/Auto-Cookies.js
@@ -0,0 +1,11 @@
+function eventFire(el, etype){
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
+
+setInterval(function(){ eventFire(document.getElementById('bigCookie'), 'click'); }, 50);
