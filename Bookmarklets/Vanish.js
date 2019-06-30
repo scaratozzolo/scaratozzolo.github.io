fdf839e3715890ed9777deae270fa729f67c0728
Scott Caratozzolo <scaratozzolo12@gmail.com>
2019-06-30 18:05:16 -0400

    Update Vanish.js

--- Bookmarklets/Vanish.js
+++ Bookmarklets/Vanish.js
@@ -32,7 +32,7 @@ function deleteElement(){
 	if(randomElement.id != 'animatecsslink'){
 		randomElement.classList.add('animated', 'fadeOut', 'slow');
 		console.log(randomElement);
-		setTimeout(function(){ randomElement.remove(); }, 2000);
+		setTimeout(function(){ randomElement.remove(); }, 10);
 	}else{
 		deleteElement();
 	}
