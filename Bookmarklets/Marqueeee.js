10a70f3379803c4dc0302c220b6e174b02d2f6ae
Scott Caratozzolo <scaratozzolo12@gmail.com>
2019-07-01 17:31:34 -0400

    Update Marqueeee.js

--- Bookmarklets/Marqueeee.js
+++ Bookmarklets/Marqueeee.js
@@ -1,17 +1,18 @@
 var el = window.getSelection().anchorNode.parentNode;
-var atts = el.attributes;
-var nodes=[], values=[];
-for (var i = 0; i < atts.length; i++){
-    att = atts[i];
-    nodes.push(att.nodeName);
-    values.push(att.nodeValue);
-}
-console.log(nodes);
-var newEl = document.createElement('marquee');
-newEl.innerHTML = el.innerHTML;
-for(var j = 0; j < atts.length; j++) {
-    newEl.setAttribute(nodes[j], values[j]);
-}
+var s = getSelText();
+var index = el.innerHTML.indexOf(s);
+console.log(index+s.length);
+var newhtml = el.innerHTML.substring(0, index) + "<marquee>" + s + "</marquee>" + el.innerHTML.substring(index+s.toString().length);
+el.innerHTML = newhtml;
 
-// replace el with newEL
-el.parentNode.replaceChild(newEl, el);
+function getSelText() {
+    var s = '';
+    if (window.getSelection) {
+        s = window.getSelection();
+    } else if (document.getSelection) {
+        s = document.getSelection();
+    } else if (document.selection) {
+        s = document.selection.createRange().text;
+    }
+    return s;
+}
