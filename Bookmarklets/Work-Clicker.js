e1f431558d1ad8ffae8307b018524a083843835a
Scott Caratozzolo <scaratozzolo12@gmail.com>
2019-07-03 17:08:10 -0400

    bug fix

--- Bookmarklets/Work-Clicker.js
+++ Bookmarklets/Work-Clicker.js
@@ -1,4 +1,4 @@
-var version = "0.3.0";
+var version = "0.3.1";
 console.log("Work-Clicker.js v" + version);
 var WC = {};
 
@@ -102,10 +102,10 @@ function initMyBookmarklet() {
       }
     //b turn on/off auto big cookie clicking
     }else if(e.keyCode==66){
-      !bigCookieClicking;
+      bigCookieClicking = !bigCookieClicking;
     //n turn on/off auto shimmer clicking
     }else if(e.keyCode==78){
-      !shimmerClicking;
+      shimmerClicking = !shimmerClicking;
     }
   });
 
@@ -175,8 +175,8 @@ function WCToggle(id, button, state, tmp){
   }
 
   if(state == 'clicking') autoClickToggle();
-  if(state == 'bigCookieClicking') !bigCookieClicking;
-  if(state == 'shimmerClicking') !shimmerClicking;
+  if(state == 'bigCookieClicking') bigCookieClicking = !bigCookieClicking;
+  if(state == 'shimmerClicking') shimmerClicking = !shimmerClicking;
 }
 
 function autoClickToggle(){
