f9e33e17b3d54d60d411697d9fa990b5d0d1d7a3
Scott Caratozzolo <scaratozzolo12@gmail.com>
2019-07-03 13:05:47 -0400

    autoClick function, shimmer auto clicking

--- Bookmarklets/Work-Clicker.js
+++ Bookmarklets/Work-Clicker.js
@@ -1,7 +1,9 @@
 var v = "3.4.1";
 var clickspeed = 75;
 var interval = null;
-var clicking = false;
+var clicking - false;
+var bigCookieClicking = false;
+var shimmerClicking = false;
 
 if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
     var done = false;
@@ -20,72 +22,72 @@ if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
 
 
 function initMyBookmarklet() {
-  interval = setInterval(function(){ eventFire(document.getElementById('bigCookie'), 'click'); }, clickspeed);
-  clicking = true;
+  interval = setInterval(function(){ autoClick(); }, clickspeed);
+  clicking, bigCookieClicking, shimmerClicking = true;
   $(document).keydown(function(e){
-    //x
+    //x turn on/off auto click
     if(e.keyCode==88){
-	if(clicking){
-		clearInterval(interval);
-		clicking = false;
-	}else{
-		interval = setInterval(function(){ eventFire(document.getElementById('bigCookie'), 'click'); }, clickspeed);
-		clicking = true;
-	}
+    	if(clicking){
+    		clearInterval(interval);
+    		clicking = false;
+    	}else{
+    		interval = setInterval(function(){ autoClick(); }, clickspeed);
+    		clicking = true;
+    	}
     //z
     }else if(e.keyCode==90){
        eventFire(document.getElementById('upgrade0'), 'click');
     //1
     }else if(e.keyCode==49){
-	eventFire(document.getElementById('product0'), 'click');
+	     eventFire(document.getElementById('product0'), 'click');
     //2
     }else if(e.keyCode==50){
-    	eventFire(document.getElementById('product1'), 'click');
+    	 eventFire(document.getElementById('product1'), 'click');
     //3
     }else if(e.keyCode==51){
-    	eventFire(document.getElementById('product2'), 'click');
+    	 eventFire(document.getElementById('product2'), 'click');
     //4
     }else if(e.keyCode==52){
-    	eventFire(document.getElementById('product3'), 'click');
+    	 eventFire(document.getElementById('product3'), 'click');
     //5
     }else if(e.keyCode==53){
-    	eventFire(document.getElementById('product4'), 'click');
+    	 eventFire(document.getElementById('product4'), 'click');
     //6
     }else if(e.keyCode==54){
-    	eventFire(document.getElementById('product5'), 'click');
+    	 eventFire(document.getElementById('product5'), 'click');
     //7
     }else if(e.keyCode==55){
-    	eventFire(document.getElementById('product6'), 'click');
+    	 eventFire(document.getElementById('product6'), 'click');
     //8
     }else if(e.keyCode==56){
-    	eventFire(document.getElementById('product7'), 'click');
+    	 eventFire(document.getElementById('product7'), 'click');
     //9
     }else if(e.keyCode==57){
-    	eventFire(document.getElementById('product8'), 'click');
+    	 eventFire(document.getElementById('product8'), 'click');
     //0
     }else if(e.keyCode==49){
-    	eventFire(document.getElementById('product9'), 'click');
+    	 eventFire(document.getElementById('product9'), 'click');
     //- and _
     }else if(e.keyCode==189){
-    	eventFire(document.getElementById('product10'), 'click');
+    	 eventFire(document.getElementById('product10'), 'click');
     //+ and =
     }else if(e.keyCode==187){
-    	eventFire(document.getElementById('product11'), 'click');
+    	 eventFire(document.getElementById('product11'), 'click');
     //q
     }else if(e.keyCode==81){
-    	eventFire(document.getElementById('product12'), 'click');
+    	 eventFire(document.getElementById('product12'), 'click');
     //w
     }else if(e.keyCode==87){
-    	eventFire(document.getElementById('product13'), 'click');
+    	 eventFire(document.getElementById('product13'), 'click');
     //e
     }else if(e.keyCode==69){
-    	eventFire(document.getElementById('product14'), 'click');
+    	 eventFire(document.getElementById('product14'), 'click');
     //r
     }else if(e.keyCode==82){
-    	eventFire(document.getElementById('product15'), 'click');
+    	 eventFire(document.getElementById('product15'), 'click');
     //c
     }else if(e.keyCode==67){
-    	eventFire(document.getElementById('bigCookie'), 'click');
+    	 eventFire(document.getElementById('bigCookie'), 'click');
     }
   });
 }
@@ -94,11 +96,23 @@ function initMyBookmarklet() {
 function eventFire(el, etype){
   if (el.fireEvent) {
     el.fireEvent('on' + etype);
-  } else {
+  }else{
     var evObj = document.createEvent('Events');
     evObj.initEvent(etype, true, false);
     el.dispatchEvent(evObj);
   }
 }
 
+function autoClick() {
 
+  if (bigCookieClicking){
+    eventFire(document.getElementById('bigCookie'), 'click');
+  }
+
+  if(shimmerClicking){
+    if(document.getElementById('shimmer').innerHTML != ""){
+      eventFire(document.getElementsByClassName('shimmer')[0], 'click');
+    }
+  }
+
+}
