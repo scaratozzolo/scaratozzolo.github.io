97ce9e604f6aa6c3fe0d891a7c1f8528c0c007bd
Scott Caratozzolo <scaratozzolo12@gmail.com>
2019-07-07 09:13:47 -0400

    Update Vanish.js

--- Bookmarklets/Vanish.js
+++ Bookmarklets/Vanish.js
@@ -14,7 +14,6 @@ if (window.jQuery === undefined || window.jQuery.fn.jquery < jqueryv) {
     script.onload = script.onreadystatechange = function(){
         if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
             done = true;
-            initMyBookmarklet();
         }
     };
     document.getElementsByTagName("head")[0].appendChild(script);
