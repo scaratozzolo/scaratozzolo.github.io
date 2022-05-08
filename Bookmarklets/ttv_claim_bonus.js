d3ae34300641b2ff42696927b1863c9265890220
Scott Caratozzolo <scaratozzolo12@gmail.com>
2022-05-08 15:04:09 -0400

    Update ttv_claim_bonus.js

--- Bookmarklets/ttv_claim_bonus.js
+++ Bookmarklets/ttv_claim_bonus.js
@@ -1,9 +1,12 @@
 function claim_bonus(){
   const bonus_button = document.querySelector('[aria-label="Claim Bonus"]');
   if (bonus_button != null){
-    bonus_button.click()
+    bonus_button.click();
+    console.log("Claim clicked");
+  }else{
+    console.log("button not found");
   }
 }
 
-setInterval(claim_bonus, 180000);
+setInterval(claim_bonus, 240000);
 
